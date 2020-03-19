b210a84ba02925efe4a8a2315faaa3587a742c18
Adam Carballido <carballido.adam@yahoo.com>
2020-03-19 18:35:36 -0400

    Changed In Order traversal
    In order traversal prints the number as well as its key count

--- BST.java
+++ BST.java
@@ -18,14 +18,15 @@ public class BST {
 		else { root.insertChild(n); }
 	}
 	
-	// In Order traversal of the tree.
+	// In Order traversal of the tree, prints the value and its key count.
 	public void inOrder(BSTNode n) {
 		// If nothing to traverse, return
 		if(n == null) { return; }
 		
 		else {
+			// Print out left sub-tree, root, right sub-tree
 			inOrder(n.getLeft() );
-			System.out.print(n.getData() + " " );
+			System.out.print(n.getData() + "(" + n.getKeyCount() + ") " );
 			inOrder(n.getRight() );
 		}
 	}
