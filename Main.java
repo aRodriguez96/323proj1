5422f94df54f20aaecbe35ebd1fd03b2eab22e45
aRodriguez96 <albertrodriguez@Alberts-MacBook-Air.local>
2020-03-20 01:55:37 -0400

    added Trie

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 import java.util.Random;
 
+
 public class Main {
 	public static void main(String args[]) {
+		
+		/////////ADAM////////////
+		
 		BST bst = new BST();
 		
 		for(int i = 0; i < 50; i++) {
@@ -15,5 +19,48 @@ public class Main {
 		bst.inOrder();
 		System.out.println();
 		System.out.println(bst.search(26) );
+		
+		
+		/////////////////////////
+		
+		/////////ALBERT//////////
+		// Input keys (use only 'a' through 'z' and lower case) 
+        String keys[] = {"the", "a", "there", "answer", "any", 
+                         "by", "bye", "their"}; 
+       
+        String output[] = {"Not present in trie", "Present in trie"}; 
+       
+       
+        TrieNode root = new TrieNode(); 
+       
+        // Construct trie 
+        int i; 
+        for (i = 0; i < keys.length ; i++) 
+            root.insert(keys[i]); 
+       
+        // Search for different keys 
+        if(root.search("the") == true) 
+            System.out.println("the --- " + output[1]); 
+        else System.out.println("the --- " + output[0]); 
+          
+        if(root.search("these") == true) 
+            System.out.println("these --- " + output[1]); 
+        else System.out.println("these --- " + output[0]); 
+          
+        if(root.search("their") == true) 
+            System.out.println("their --- " + output[1]); 
+        else System.out.println("their --- " + output[0]); 
+          
+        if(root.search("thaw") == true) 
+            System.out.println("thaw --- " + output[1]); 
+        else System.out.println("thaw --- " + output[0]); 
+		
+		
+		
+	
+		
+		
+		/////////////////////////
+		
 	}
 }
