851c28b15162fd915937f33ca066927464d30013
aRodriguez96 <44889556+aRodriguez96@users.noreply.github.com>
2020-03-20 01:59:23 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,36 +24,37 @@ public class Main {
 		/////////////////////////
 		
 		/////////ALBERT//////////
+		
 		// Input keys (use only 'a' through 'z' and lower case) 
-        String keys[] = {"the", "a", "there", "answer", "any", 
-                         "by", "bye", "their"}; 
-       
-        String output[] = {"Not present in trie", "Present in trie"}; 
-       
-       
-        TrieNode root = new TrieNode(); 
-       
-        // Construct trie 
-        int i; 
-        for (i = 0; i < keys.length ; i++) 
-            root.insert(keys[i]); 
-       
-        // Search for different keys 
-        if(root.search("the") == true) 
-            System.out.println("the --- " + output[1]); 
-        else System.out.println("the --- " + output[0]); 
-          
-        if(root.search("these") == true) 
-            System.out.println("these --- " + output[1]); 
-        else System.out.println("these --- " + output[0]); 
-          
-        if(root.search("their") == true) 
-            System.out.println("their --- " + output[1]); 
-        else System.out.println("their --- " + output[0]); 
-          
-        if(root.search("thaw") == true) 
-            System.out.println("thaw --- " + output[1]); 
-        else System.out.println("thaw --- " + output[0]); 
+		String keys[] = {"the", "a", "there", "answer", "any", 
+				 "by", "bye", "their"}; 
+
+		String output[] = {"Not present in trie", "Present in trie"}; 
+
+
+		TrieNode root = new TrieNode(); 
+
+		// Construct trie 
+		int i; 
+		for (i = 0; i < keys.length ; i++) 
+		    root.insert(keys[i]); 
+
+		// Search for different keys 
+		if(root.search("the") == true) 
+		    System.out.println("the --- " + output[1]); 
+		else System.out.println("the --- " + output[0]); 
+
+		if(root.search("these") == true) 
+		    System.out.println("these --- " + output[1]); 
+		else System.out.println("these --- " + output[0]); 
+
+		if(root.search("their") == true) 
+		    System.out.println("their --- " + output[1]); 
+		else System.out.println("their --- " + output[0]); 
+
+		if(root.search("thaw") == true) 
+		    System.out.println("thaw --- " + output[1]); 
+		else System.out.println("thaw --- " + output[0]); 
 		
 		
 		
